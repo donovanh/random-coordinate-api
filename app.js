d389aae9488bfcadcd5283c873b8f14eadf122de
Donovan Hutchinson <donovan@kitmanlabs.com>
2016-06-12 12:52:37 +0100

    Adding origins

--- app.js
+++ app.js
@@ -3,13 +3,7 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    next();
-  }
-);
+app.use(cors());
 
 app.get('/', function(req, res) {
   res.send('World Map API - opening a socket');
