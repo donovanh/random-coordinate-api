2468301cc282ca4a64d949306c2432845f66ce65
Donovan Hutchinson <donovan@kitmanlabs.com>
2016-06-12 12:50:29 +0100

    Adding origins

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*/*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     next();
