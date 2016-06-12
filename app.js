5d21e5ad4ed9384ed0dba4b81a6b4e22b0c368ac
Donovan Hutchinson <donovan@kitmanlabs.com>
2016-06-12 12:57:36 +0100

    Adding access control allow stuff

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 'use strict';
 const app = require('express')();
 const http = require('http').Server(app);
-const cors = require('cors');
 const io = require('socket.io')(http);
 
-app.use(cors());
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', false);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  }
+);
 
 app.get('/', function(req, res) {
   res.send('World Map API - opening a socket');
