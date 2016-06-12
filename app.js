5b67d2cc9cc992e08abc7553e01913c50ba701d8
Donovan Hutchinson <donovan@kitmanlabs.com>
2016-06-12 12:44:32 +0100

    Adding origins

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+io.set('origins', '*:*');
+
 app.get('/', function(req, res) {
   res.send('World Map API - opening a socket');
 });
